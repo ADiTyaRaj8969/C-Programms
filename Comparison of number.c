9703243c366ea527656f23bf4119328ca93925fb
ADiTyaRaj8969 <147165909+ADiTyaRaj8969@users.noreply.github.com>
2023-10-06 19:39:49 +0530

    Add files via upload

---
+++ Comparison of number.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  float a,b;
+   printf("ENTER THE VALUE OF a -:\n");
+   scanf("%f",&a);
+   printf("ENTER THE VALUE OF b-:\n");
+   scanf("%f",&b);
+   a<b?printf("b is greater-:"):printf("a is greater");
+     return 0;
+}
